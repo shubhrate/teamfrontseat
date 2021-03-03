7ef2f30d3fcf3bc8a145e00e8d4e687dc3249f83
Shubhra Tewari <tewaris@whitman.edu>
2021-03-02 21:52:54 -0800

    remove code to insert data into database - this will be handled in webclient.js

--- backendfiles/app.js
+++ backendfiles/app.js
@@ -51,12 +51,6 @@ mongoose.connect(
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const User = require('./models/User');
-var user_instance = new User({ name: "Tester", password: "password" });
-user_instance.save(function (err) {
-    if (err) console.log(err);
-});
-
 //test write and test read functions (mongoose.)
 
 //Start Listening to Server:
