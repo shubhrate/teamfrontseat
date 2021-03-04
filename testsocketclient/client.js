5b365087cc67c10308128c432493176e08ecd8df
Shubhra Tewari <tewaris@whitman.edu>
2021-03-04 14:04:02 -0800

    comment out code causing errors to be dealt with later

--- testsocketclient/client.js
+++ testsocketclient/client.js
@@ -13,12 +13,16 @@ webSocket.onerror = onSocketError;
 // I expect to receive a WebSocket event object in response
 function onSocketOpened(evt) {
   alert("Open socket");
-	document.getElementById("BUTTON_SEND_MESSAGE").addEventListener("click", onSendMessage);
+  document.getElementById("BUTTON_SEND_MESSAGE").addEventListener("click", onSendMessage);
 }
 
 function onSocketError(evt)
 {
   // evt contains error info
+}
+
+function isOpen(ws) {
+  return ws.readyState === ws.OPEN;
 }
 
 // I receive nothing from Web Browser due to user clicking button.
@@ -29,6 +33,10 @@ function onSendMessage()
   let messg = {type: "post", collection: "users", data: {name: "Jane", password: "password", id: "487434"}};
 	let jsonMessg = JSON.stringify( messg );
 	console.log("Sending " + jsonMessg);
+  if (!isOpen(webSocket)) {
+    console.log("Socket is closed whoops");
+    return;
+  }
   webSocket.send( jsonMessg );
 }
 
@@ -38,8 +46,9 @@ function onSocketReceiveMessage(evt)
 {
   // consumes event data and converts JSON from server to JS object.
 	// Only one chance to consume message so parse, then use it.
-  let messg = JSON.parse(evt.data);
+  //let messg = JSON.parse(evt.data); - this is causing an error right now so commented out
 
 	// if(messg.type == "get_all" && messg.result == "OK")
-	  alert( JSON.stringify( messg ) );
+	//  alert( JSON.stringify( messg ) ); 
+  console.log(evt);
 }
