d3141e811bf9dfd5fec439a09fb90a7716626c33
Shubhra Tewari <tewaris@whitman.edu>
2021-03-04 17:12:50 -0800

    resolve merge conflict

--- backendfiles/models/User.js
+++ backendfiles/models/User.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
     name: {
-<<<<<<< HEAD
         type: String,
         required: false
-=======
-            type: String,
-            required: false,
->>>>>>> 210d82724b5606038bf0a7c9c3376eeb8616c106
     },
     id: {
         type: String,
