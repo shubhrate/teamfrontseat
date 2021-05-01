52078a2ef00507b0a02ef8881c4832cfacfeeb6a
Shubhra Tewari <tewaris@whitman.edu>
2021-05-01 12:18:41 -0700

    extract code into functions and set to run them onload of mainBody

--- client/main.js
+++ client/main.js
@@ -1,80 +1,65 @@
 import Diagram from "./js/diagram.js";
 import InputManager from "./js/inputmanager.js";
-import Animator from "./js/animator.js";
 import * as client from "./js/webclient.js";
-
 import {address} from "./js/_addr.js";
 
-//SET UP DIAGRAM///////////////////////////////////////////
+document.getElementById("mainBody").onload = function startProgram() {
+	const canvas = document.getElementById("diagram");
+	const diagram = new Diagram("1", canvas);
+	drawDiagram();
+	addResizeEventListener();
+	openClientConnection();
 
-const canvas = document.getElementById("diagram");
-const diagram = new Diagram("1", canvas);
-//TODO / NOTE: diagramID will likely be in uniqueID format later
-client.registerDiagram(diagram);
-diagram.resizeToFill();
-diagram.windowX = diagram.width / diagram.scale / 2;
-diagram.windowY = diagram.height / diagram.scale / 2;
+	//SET UP DIAGRAM///////////////////////////////////////////
 
-new InputManager(diagram);
-diagram.draw();
-
-//RESIZE MANAGER!//////////////////////////////////////////
-
-const RESIZE_INTERVAL = 250;
-let blockResize = false;
-let resizeQueued = false;
-
-function unblockResize() {
-	if(resizeQueued) {
+	//TODO / NOTE: diagramID will likely be in uniqueID format later
+	function drawDiagram(){
+		client.registerDiagram(diagram);
 		diagram.resizeToFill();
-		resizeQueued = false;
-		window.setTimeout(unblockResize, RESIZE_INTERVAL);
-	} else {
-		blockResize = false;
-	}
-}
-
-function resize() {
-	if (blockResize) {
-		resizeQueued = true;
-	} else {
-		diagram.resizeToFill();
-		blockResize = true;
-		window.setTimeout(unblockResize, RESIZE_INTERVAL);
-	}
-}
-
-window.addEventListener("resize", resize);
-
-//CONNECT TO SERVER, GET DIAGRAM CONTENTS//////////////////
-
-client.open(address, () => {
-	client.send({
-		type: "getAll",
-		collection: "entities",
-		data: {diagramID: "1"}
-	}, function(data) {
-		diagram.addUnclassifiedEntity(...data.result);
+		diagram.windowX = diagram.width / diagram.scale / 2;
+		diagram.windowY = diagram.height / diagram.scale / 2;
+	
+		new InputManager(diagram);
 		diagram.draw();
-	});
-	/*
-	let player = {
-		type: "new player",
-		//id: "Jane Doe",
-		collection: "users",
-		data: {
-			name: "Jane",
-			mojoPort: 9003,
-			mojoIpAddress: "localHost",
-			id: "178376c1f97-f0fa6018"
+	}
+	
+	//RESIZE MANAGER!//////////////////////////////////////////
+	
+	const RESIZE_INTERVAL = 250;
+	let blockResize = false;
+	let resizeQueued = false;
+	
+	function unblockResize() {
+		if(resizeQueued) {
+			diagram.resizeToFill();
+			resizeQueued = false;
+			window.setTimeout(unblockResize, RESIZE_INTERVAL);
+		} else {
+			blockResize = false;
 		}
-	};
-	client.send(player);
-	*/
-});
-
-/*
-const animator = new Animator(diagram);
-animator.animateCross(diagram.entities[3], 2000, 0, -3, Math.PI, 500);
-animator.animatePath(diagram.entities[2], 2000, [1, 3, 2], [3, 2, 1], 0, (x) => Math.pow(x, 3));
-*/
+	}
+	
+	function resize() {
+		if (blockResize) {
+			resizeQueued = true;
+		} else {
+			diagram.resizeToFill();
+			blockResize = true;
+			window.setTimeout(unblockResize, RESIZE_INTERVAL);
+		}
+	}
+	
+	function addResizeEventListener(){
+		window.addEventListener("resize", resize);
+	}
+	
+	//CONNECT TO SERVER, GET DIAGRAM CONTENTS//////////////////
+	function openClientConnection() {
+		client.open(address, () => {
+			if (document.URL.includes("edit.html")) { //one edit HTML page for all possible edit options
+				client.addEditEventListeners();
+			}
+			client.refreshDiagram(diagram);
+		});
+	}
+}
